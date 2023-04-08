50ba9c34f6370decd431333d17a7faf79c6781f2
mattboentoro <mattboentoro@yahoo.com>
2023-04-08 16:59:30 -0700

    Change the format of the HTML

--- resources/lambda.js
+++ resources/lambda.js
@@ -1,7 +1,7 @@
 const aws = require("aws-sdk");
 const nodemailer = require("nodemailer");
 
-async function sendEmail(senderEmail, senderName, message, subject, date, email) {
+async function sendEmail(senderEmail, senderName, message, subject, date) {
   let transporter = nodemailer.createTransport({
     SES: new aws.SES({ region: "us-west-2", apiVersion: "2010-12-01" }),
   });
@@ -10,8 +10,46 @@ async function sendEmail(senderEmail, senderName, message, subject, date, email)
       to: "mattboentoro@yahoo.com",
       subject: `[From MyWebsite] ${subject}`,
       text: message,
-      html: `<html><head><style> .title{ font-weight: 800;} .message { margin-top: 20px;}</style></head><body><div><span class='title'>Sender: </span><span>${senderName} (${senderEmail})</span></div><div><span class='title'>Subject: </span><span>${subject}</span></div><div><span class='title'>Date: </span><span>${date}</span></div><div class='message'>${message.replace(/\n/g,'<br/>')}</div></body></html>`,
+      html: generateHTML(senderEmail, senderName, message, subject, date),
   });
+}
+
+function generateHTML(senderEmail, senderName, message, subject, date) {
+    return `
+        <html>
+            <head>
+            <style> 
+            * {
+                font-family: sans-serif;
+            }
+            .title { 
+                font-weight: 700;
+            } 
+            .message { 
+                margin-top: 20px;
+            }
+            .title { 
+                display: inline-block; width: 100px; 
+            }
+            .title-div {
+                font-size: 16px
+            }
+            </style>
+            </head>
+            <body>
+            <div class='title-div'>
+                <span class='title'>👨 Sender: </span><span>${senderName} (${senderEmail})</span>
+            </div>
+            <div class='title-div'>
+                <span class='title'>📢 Subject: </span><span>${subject}</span>
+            </div>
+            <div class='title-div'>
+                <span class='title'>📅 Date: </span><span>${date}</span>
+            </div>
+            <div class='message'>${message.replace(/\n/g,'<br/>')}</div>
+            </body>
+        </html>
+    `
 }
 
 exports.main = async (event) => {
