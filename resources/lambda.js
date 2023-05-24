33d29cd52419d914e88ffdbff8a6eedb464c0a97
mattboentoro <mattboentoro@yahoo.com>
2023-05-23 22:13:01 -0700

    change layout, add timezone field

--- resources/lambda.js
+++ resources/lambda.js
@@ -1,7 +1,7 @@
 const aws = require("aws-sdk");
 const nodemailer = require("nodemailer");
 
-async function sendEmail(senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region) {
+async function sendEmail(senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region, time, timezone) {
   let transporter = nodemailer.createTransport({
     SES: new aws.SES({ region: "us-west-2", apiVersion: "2010-12-01" }),
   });
@@ -10,7 +10,7 @@ async function sendEmail(senderEmail, senderName, message, subject, date, ipAddr
       to: "mattboentoro@yahoo.com",
       subject: `🛎️ [From MyWebsite] ${subject}`,
       text: message,
-      html: generateHTML(senderEmail, senderName, message, date, ipAddress, country, countryCode, region),
+      html: generateHTML(senderEmail, senderName, message, date, ipAddress, country, countryCode, region, time, timezone),
       replyTo: senderEmail
   });
 }
@@ -23,7 +23,7 @@ function getFlagEmoji(countryCode) {
     return String.fromCodePoint(...codePoints);
   }
 
-function generateHTML(senderEmail, senderName, message, date, ipAddress, country, countryCode, region) {
+function generateHTML(senderEmail, senderName, message, date, ipAddress, country, countryCode, region, time, timezone) {
     return `
         <html>
             <head>
@@ -34,15 +34,35 @@ function generateHTML(senderEmail, senderName, message, date, ipAddress, country
                 }
                 .title { 
                     font-weight: 700;
+                    
                 } 
                 .message { 
                     margin-top: 20px;
                 }
                 .title { 
-                    display: inline-block; width: 30px; 
+                    display: inline-block; 
+                    width: 30px; 
+
                 }
                 .sender-detail {
-                font-weight: 700;
+                  font-weight: 700;
+                  background-color: #149ddd;
+                  border-radius: 15px;
+                  padding-top: 5px;
+                  padding-bottom: 5px;
+                  padding-left: 8px;
+                  padding-right: 10px;
+                  color: white;
+                  display: inline-block;
+                  font-size: 11px;
+                  margin-right: 2px;
+                  margin-bottom: 6px;
+                  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.2);
+                }
+                a {
+                  font-size: 11px;
+                  color: white;
+                  text-decoration: none;
                 }
             </style>
             </head>
@@ -51,19 +71,14 @@ function generateHTML(senderEmail, senderName, message, date, ipAddress, country
                     ${message.replace(/\n/g,'<br/>')}
                 </div>
                 <div class='title-div'>
-                    <span class='title'>👨 </span><span class='sender-detail'>${senderName} (<a href = "mailto: ${senderEmail}">${senderEmail}</a>)</span>
-                </div>
-                <div class='title-div'>
-                    <span class='title'>📅 </span><span class='sender-detail'>${date}</span>
-                </div>
-                <div class='title-div'>
-                    <span class='title'>🚩 </span><span class='sender-detail'>${ipAddress}</span>
-                </div>
-                <div class='title-div'>
-                    <span class='title'>📌 </span><span class='sender-detail'>${region}</span>
-                </div>
-                <div class='title-div'>
-                    <span class='title'>🌏 </span><span class='sender-detail'>${country} ${getFlagEmoji(countryCode)}</span>
+                    <span class='sender-detail'>👨&nbsp;&nbsp;${senderName}</span>
+                    <span class='sender-detail'>📨&nbsp;&nbsp;<a href = "mailto: ${senderEmail}">${senderEmail}</a></span>
+                    <span class='sender-detail'>📅&nbsp;&nbsp;${date}</span>
+                    <span class='sender-detail'>🕰️&nbsp;&nbsp;${time}</span>
+                    <span class='sender-detail'>️⏳&nbsp;&nbsp;${timezone}</span>
+                    <span class='sender-detail'>🚩&nbsp;&nbsp;${ipAddress}</span>
+                    <span class='sender-detail'>📌&nbsp;&nbsp;${region}</span>
+                    <span class='sender-detail'>🌏&nbsp;&nbsp;${country} ${getFlagEmoji(countryCode)}</span>
                 </div>
                 <div class='message'>${message.replace(/\n/g,'<br/>')}</div>
             </body>
@@ -72,12 +87,12 @@ function generateHTML(senderEmail, senderName, message, date, ipAddress, country
 }
 
 exports.main = async (event) => {
-  const { senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region } = JSON.parse(
+  const { senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region, time, timezone } = JSON.parse(
     event.body
   );
 
   try {
-    await sendEmail(senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region);
+    await sendEmail(senderEmail, senderName, message, subject, date, ipAddress, country, countryCode, region, time, timezone);
     return {
       statusCode: 200,
       message: "Email sent successfully!"
